8ef052669fba1604956d52793c38e523663cd0af
david wee <rook2pawn@gmail.com>
2012-03-07 14:11:43 -0800

    First commit. v0.0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+node-login-express
+==================
+
+Learn yourself a simple login with Express, Formidable, Connect-Store using Redis and EJS template. 
+
+Installation
+============
+
+    You have to install redis first. If you are on a linux system use your package manage to "install redis".
+
+    Then, run server.js and open your web browser to http://localhost:4000
+
+Goals
+=====
+
+    To make a clean skeleton for login/logout/registration for others to learn from.
+
+That's all! Have a good day.
---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var argv = require('optimist')
+    .usage('Usage: $0 --port=<portnum>\nvisit localhost:port')
+    .alias('p', 'port')
+    .describe('p', 'Specify the port you want the server to run on')
+    .argv
+;
+var formidable = require('formidable');
+var fs = require('fs');
+var Redis = require('connect-redis')(express);
+var redisClient = require("redis"), client = redisClient.createClient();
+client.on("error", function (err) {
+    console.log("redis client Error " + err);
+});
+var auth = function(req,name) {
+    req.session.auth = true;
+    req.session.user = name;
+};
+var server = express.createServer(
+    express.cookieParser(),
+    express.session({ 
+        secret: "keyboard cat",
+        store: new Redis
+    })
+);
+server.get('/', function (req, res) {
+    res.render('index.ejs',{
+        layout:false,
+        error : req.session.error,
+        auth : req.session.auth,
+        user : req.session.user,
+        host : req.headers.host
+    });
+}); 
+server.post('/logout', function(req,res) {
+    req.session.destroy(function(err) {
+        res.redirect('back');
+    });
+});
+server.post('/login', function(req,res) {
+    var form = new formidable.IncomingForm();
+    req.session.error = {};
+    form.parse(req, function(err, fields, files) {
+        var name = fields.name; var password = fields.password;
+        client.hexists("users", name, function(err, replies) {
+            if (replies == 1) {
+                client.hget("users", name, function(err, obj) {
+                    var hash = JSON.parse(obj);
+                    if (password === hash.password) {
+                        auth(req,name);
+                    } else {
+                        req.session.error.login = true;
+                        req.session.error.password = true;
+                    }
+                    res.redirect('back');
+                });
+            } else {
+                req.session.error.login = true;
+                req.session.error.username = true;
+                res.redirect('back');
+            } 
+        });
+    }); 
+});
+server.post('/register', function(req,res) {
+    var form = new formidable.IncomingForm();
+    req.session.error = {};
+    form.parse(req, function(err, fields, files) {
+        if ((fields.name !== undefined) && (fields.password !== undefined)) {
+            var name = fields.name;
+            var password = fields.password;
+            client.hgetall("users", function (err, obj) {
+                if (obj[name] === undefined) {
+                    var data = {name:name, password:password};
+                    client.hmset("users", name, JSON.stringify(data));
+                    auth(req,name);
+                } else {
+                    req.session.error = {};
+                    req.session.error.register = true;
+                }
+                res.redirect('back');
+            });
+        } 
+    }); 
+});
+
+var port = argv.port || argv.p || 4000;
+console.log('Listening on ' + port);
+server.listen(port);
